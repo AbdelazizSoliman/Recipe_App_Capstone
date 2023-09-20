d312920eba73d420a3178c36ea7aba2a2cb09992
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-20 14:18:37 +0300

    Add some validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 250 }
   has_many :foods
   has_many :recipes
 end
